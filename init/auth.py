7002d85d568d3ea899c999109f5a05a3aaa747be
Joost VanderBorgh <joostv@sas.upenn.edu>
2021-07-23 20:51:11 +0200

    working

--- init/auth.py
+++ init/auth.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, flash, request
+from flask import render_template, Blueprint, flash, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -10,7 +10,7 @@ auth = Blueprint('auth',__name__)
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        first_name = request.form.get('userName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -32,26 +32,36 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('views.upload_page'))
 
-    return render_template("sign_up.html", user=current_user)
+    return render_template("signup.html", user=current_user)
     
 # decerator for login
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
-        password1 = request.form.get('password1')
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.upload_page'))
+            else:
+                flash('Incorrect password, try again.', category='error')
         else:
-            flash('Account created!', category='success')
-    return render_template("login.html")
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 # decerator for logout
-@auth.route('/logout', methods=['POST', 'GET'])
+@auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 UPLOAD_FOLDER = '/static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -75,26 +85,26 @@ def ocr_core(filename):
     return text
 
 # route and function to handle the upload page
-@auth.route('/upload', methods=['GET', 'POST'])
+@auth.route('/', methods=['GET', 'POST'])
 def upload_page():
     if request.method == 'POST':
         # check if there is a file in the request
         if 'file' not in request.files:
-            return render_template('upload.html', msg='No file selected')
+            return render_template('home.html', msg='No file selected')
         file = request.files['file']
         # if no file is selected
         if file.filename == '':
-            return render_template('upload.html', msg='No file selected')
-
+            return render_template('home.html', msg='No file selected')
+        print(file.filename)
         if file and allowed_file(file.filename):
 
             # call the OCR function on it
             extracted_text = ocr_core(file)
-
+            print(extracted_text)
             # extract the text and display it
             return render_template('upload.html',
                                    msg='Successfully processed',
                                    extracted_text=extracted_text,
                                    img_src=UPLOAD_FOLDER + file.filename)
     elif request.method == 'GET':
-        return render_template('upload.html')
+        return render_template('home.html')
--- init/views.py
+++ init/views.py
@@ -1,17 +1,98 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
 views = Blueprint('views', __name__)
-@views.route('/')
-def home():
-    return render_template("home.html")
+# @views.route('/')
+# @login_required
+# def home():
+#    return render_template("home.html", user=current_user)
 
-@views.route('/sign-up')
-def signup():
-    return render_template("signup.html")
-    
-@views.route('/login')
-def login():
-    return render_template("login.html")
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = '/static/uploads/'
 
-@views.route('/logout')
-def logout():
-    return render_template("logout.html")
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+import pytesseract
+from PIL import Image
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+# route and function to handle the upload page
+@views.route('/', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+      if request.form.get('note') is not None:
+         note = request.form.get('note')
+         input_note = note
+         if len(note) < 1:
+            flash('Note is too short!', category='error')
+         else:
+            neighbor_count = {}
+            note = "".join(char for char in note if char not in ('!','.',':','.',",","^","/","!","$","%","*",";",":",")","(","[")).lower()
+            words = note.split()
+            print(words)
+            for index, word in enumerate(words):
+               if index+1 < len(words):
+                        if word > words[index+1]:
+                           wordPair = (word, words[index+1])
+                        else:
+                           wordPair = (words[index+1], word)
+                        if wordPair in neighbor_count:
+                           neighbor_count[wordPair] += 1
+                        else:
+                           neighbor_count[wordPair]  = 1
+            neighbor_count = str(neighbor_count)
+            new_note = Note(data=neighbor_count, input=input_note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+      # check if there is a file in the request
+      if 'file' not in request.files:
+         return render_template('home.html', msg='No file selected',user=current_user)
+      file = request.files['file']
+      # if no file is selected
+      if file.filename == '':
+         return render_template('home.html', msg='No file selected', user=current_user)
+      print(file.filename)
+      if file and allowed_file(file.filename):
+
+         # call the OCR function on it
+         extracted_text = ocr_core(file)
+         print(extracted_text)
+         # extract the text and display it
+         return render_template('home.html', user=current_user,
+                                 msg='Successfully processed',
+                                 extracted_text=extracted_text,
+                                 img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('home.html',user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
+
+@views.route('/reuse-note', methods=['POST'])
+def reuse_note():
+    flash('Input copied!', category='success')
+    return jsonify({})
